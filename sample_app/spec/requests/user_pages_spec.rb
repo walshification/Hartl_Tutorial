a73315c6364d9da019d05dd4321530c9d918a397
walshification <walshification@gmail.com>
2014-06-22 11:59:26 -0500

    Add user microposts

--- sample_app/spec/requests/user_pages_spec.rb
+++ sample_app/spec/requests/user_pages_spec.rb
@@ -52,14 +52,6 @@ describe "User pages" do
     end
   end
   
-  describe "profile page" do
-    let(:user) { FactoryGirl.create(:user) }
-    before { visit user_path(user) }
-
-    it { should have_content(user.name) }
-    it { should have_title(user.name) }
-  end
-  
   describe "signup page" do
     before { visit signup_path }
 
@@ -85,6 +77,23 @@ describe "User pages" do
 		it { should have_content('error') }
 	  end
     end
+  
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
+  end
 
     describe "with valid information" do
       before do
